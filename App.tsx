c7bd569451d17229432edcbbb7c4dacb152c6548
Adarsh Pandey <adarshpnd71@gmail.com>
2024-03-19 22:53:09 +0530

    feat: making a conference automation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,42 @@
-import {View, Text, NativeModules} from 'react-native';
-import React from 'react';
+import {View, Text, NativeModules, PermissionsAndroid} from 'react-native';
+import React, {useEffect} from 'react';
 const callModule = NativeModules.CallModule;
 
 const App = () => {
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CALL_PHONE,
+        {
+          title: 'Cool Photo App Camera Permission',
+          message:
+            'Cool Photo App needs access to your camera ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
     <View>
       <Text
-        onPress={() =>
-          callModule.showToast('Adarsh is the greatest programmer')
-        }>
+        onPress={() => {
+          callModule.showToast('Adarsh is the greatest programmer');
+          callModule.makeCall('9984703591');
+        }}>
         App
       </Text>
     </View>
--- android/app/src/main/java/com/dialer/CallModule.kt
+++ android/app/src/main/java/com/dialer/CallModule.kt
@@ -1,5 +1,7 @@
 package com.dialer
 
+import android.content.Intent
+import android.net.Uri
 import android.view.Gravity
 import android.widget.Toast
 import com.facebook.react.bridge.ReactApplicationContext
@@ -16,5 +18,13 @@ class CallModule (private val reactContext: ReactApplicationContext): ReactConte
         val toast = Toast.makeText(reactContext, message, Toast.LENGTH_SHORT)
         toast.setGravity(Gravity.CENTER, 0, 0)
         toast.show()
+    }
+
+    @ReactMethod
+    fun makeCall(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_CALL)
+        intent.data = Uri.parse("tel:$phoneNumber")
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        reactApplicationContext.startActivity(intent)
     }
 }
